4f99faedb883b8e737afbe3bc53fa982baf695c6
hhsNel <hihijsonel@gmail.com>
2026-07-10 17:57:49 +0200

    fix:s1alib strln

---
+++ stage0/tests/memcp/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eo pipefail
+
+echo "memcp" >&2
+
+asm="$1"
+epack="$2"
+s1alib="$3"
+
+if [[ ! -f "$asm" || ! -f "$epack" || ! -f "$s1alib" ]]; then
+	echo "$0 <assembler> <epack> <s1alib>"
+	exit 1
+fi
+
+source ../common.sh "$asm" "$epack" "$s1alib"
+trap stop EXIT
+
+assemble_from code.s1a
+
+run | assert_str "Hello World\n"
+
+assert_ec0
+
